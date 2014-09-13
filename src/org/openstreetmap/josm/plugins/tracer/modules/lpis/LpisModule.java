f4afc4fb433202771e93552c929d5720c83363ec
Martin Svec <osm@maatts.cz>
2014-09-13 12:45:00 +0200

    Split LPIS tracing to async download and synchronous polygon edits
    Also cleanup LpisModule class and tracer code...

--- src/org/openstreetmap/josm/plugins/tracer/modules/lpis/LpisModule.java
+++ src/org/openstreetmap/josm/plugins/tracer/modules/lpis/LpisModule.java
@@ -58,16 +58,10 @@ import org.xml.sax.SAXException;
 
 public class LpisModule implements TracerModule  {
 
-    private final long serialVersionUID = 1L;
-
-    protected boolean cancel;
     private boolean moduleEnabled;
-    private String  source = "lpis";
 
-    private StringBuilder msg = new StringBuilder();
-    private ConnectWays connectWays = new ConnectWays();
-
-    protected LpisServer server = new LpisServer();
+    private final String source = "lpis";
+    private final String lpisUrl = "http://eagri.cz/public/app/wms/plpis_wfs.fcgi";
 
     public LpisModule(boolean enabled) {
       moduleEnabled = enabled;
@@ -103,29 +97,21 @@ public class LpisModule implements TracerModule  {
         private final boolean m_alt;
         private final boolean m_shift;
 
+        private LpisRecord m_record;
+        private Exception m_asyncException;
+
         LpisTracerTask (LatLon pos, boolean ctrl, boolean alt, boolean shift) {
             super (tr("Tracing"));
             this.m_pos = pos;
             this.m_ctrl = ctrl;
             this.m_alt = alt;
             this.m_shift = shift;
+            this.m_record = null;
+            this.m_asyncException = null;
         }
 
         protected void realRun() throws SAXException {
             ProgressMonitor pm = progressMonitor.createSubTaskMonitor(ProgressMonitor.ALL_TICKS, false);
-            final Collection<Command> commands = new LinkedList<Command>();
-            TracerPreferences pref = TracerPreferences.getInstance();
-
-            String sUrl = "http://eagri.cz/public/app/wms/plpis_wfs.fcgi";
-            double dAdjX = 0, dAdjY = 0;
-
-//         if (pref.m_customRuianUrl)
-//           sUrl = pref.m_customRuianUrlText;
-//
-//         if (pref.m_ruianAdjustPosition) {
-//           dAdjX = pref.m_ruianAdjustPositionLat;
-//           dAdjY = pref.m_ruianAdjustPositionLon;
-//         }
 
             System.out.println("");
             System.out.println("-----------------");
@@ -134,175 +120,189 @@ public class LpisModule implements TracerModule  {
             System.out.println("");
             pm.beginTask(null, 3);
 
-            LpisRecord record;
-
             try {
-                record = server.getElementBasicData(m_pos, sUrl);
-                System.out.println("  LPIS ID: " + record.getLpisID());
-                System.out.println("  LPIS usage: " + record.getUsage());
-                if (record.getLpisID() == -1) {
-                    TracerUtils.showNotification(tr("Data not available.")+ "\n(" + m_pos.toDisplayString() + ")", "warning");
-                    return;
-                }
-
-                Way outer = new Way();
-                Relation rel = new Relation();
-
-                // Create Outer way
-                List<Bounds> dsBounds = Main.main.getCurrentDataSet().getDataSourceBounds();
-                Node firstNode = null;
-                // record.getCoorCount() - 1 - ommit last node
-                for (int i = 0; i < record.getOuter().size() - 1; i++) {
-                    Node node;
-//                 // Apply corrections to node coordinates
-//                 if (!pref.m_ruianAdjustPosition) {
-                    node = new Node(record.getOuter().get(i));
-//                 } else {
-//                   node = new Node(new LatLon(LatLon.roundToOsmPrecisionStrict(record.getCoor(i).lat()+dAdjX),
-//                                              LatLon.roundToOsmPrecisionStrict(record.getCoor(i).lon()+dAdjY)));
-//                 }
-                    if (firstNode == null) {
-                        firstNode = node;
-                    }
-                // Check. whether traced node is inside downloaded area
-                    int insideCnt = 0;
-                    for (Bounds b: dsBounds) {
-                        if (b.contains(node.getCoor())) {
-                            insideCnt++;
-                        }
-                    }
-                    if (insideCnt == 0) {
-                        ExtendedDialog ed = new ExtendedDialog(
-                            Main.parent, tr("Way is outside downloaded area"),
-                                new String[] {tr("Ok")});
-                        ed.setButtonIcons(new String[] {"ok"});
-                        ed.setIcon(JOptionPane.ERROR_MESSAGE);
-                        ed.setContent(tr("Sorry.\nThe traced way (or part of the way) is outside of the downloaded area.\nPlease download area around the way and try again."));
-                        ed.showDialog();
-                        return;
-                    }
-                    commands.add(new AddCommand(node));
-                    outer.addNode(node);
-                }
-
-                // Insert first node again - close the polygon.
-                outer.addNode(firstNode);
-
-                System.out.println("TracedWay: " + outer.toString());
-                if (! record.hasInners()) {
-                    tagOuterWay(record, outer);
-                }
-
-                commands.add(new AddCommand(outer));
-
-                if (record.hasInners()) {
-                    // Inners found - create multipolygon
-
-                    rel.addMember(new RelationMember("outer", outer));
-
-                    for (int i = 0; i < record.getInnersCount(); i++) {
-                        ArrayList<LatLon> in = record.getInner(i);
-                        Way inner = new Way();
-                        firstNode = null;
-                        for (int j = 0; j < in.size() -1 ; j++) {
-                            Node node;
-  //                        // Apply corrections to node coordinates
-  //                        if (!pref.m_ruianAdjustPosition) {
-                            node = new Node(in.get(j));
-  //                        } else {
-  //                            node = new Node(new LatLon(LatLon.roundToOsmPrecisionStrict(record.getCoor(i).lat()+dAdjX),
-  //                                              LatLon.roundToOsmPrecisionStrict(record.getCoor(i).lon()+dAdjY)));
-  //                        }
-                            if (firstNode == null) {
-                                firstNode = node;
-                            }
-                            commands.add(new AddCommand(node));
-                            inner.addNode(node);
-                        }
-
-                        // Insert first node again - close the polygon.
-                        inner.addNode(firstNode);
-
-                        System.out.println("Traced Inner Way: " + inner.toString());
-
-                        commands.add(new AddCommand(inner));
-                        rel.addMember(new RelationMember("inner", inner));
-                    }
-
-                    // Add relation
-                    tagMultipolygon(record, rel);
-                    commands.add(new AddCommand(rel));
-                }
-
-                // connect to other landuse or modify existing landuse
-                Command connCmd;
-                if (record.hasInners()) {
-                    connCmd = connectWays.connect(outer, rel, m_pos, m_ctrl, m_alt, source);
-                } else {
-                    connCmd = connectWays.connect(outer, m_pos, m_ctrl, m_alt, source);
-                }
-
-                String s[] = connCmd.getDescriptionText().split(": ");
-
-                if (s[1].equals("Nothing")) {
-                    TracerUtils.showNotification(tr("Nothing changed."), "info");
-
-                    if (m_shift) {
-                        Main.main.getCurrentDataSet().addSelected(record.hasInners()?rel:outer);
-                    } else {
-                        Main.main.getCurrentDataSet().setSelected(record.hasInners()?rel:outer);
-                    }
-                } else {
-                    commands.add(connCmd);
-
-                    if (!commands.isEmpty()) {
-                        String msg = tr("Tracer(LPIS): add an area") + " \"" + record.getUsage() + "\"";
-                        if (record.hasInners()) {
-                            msg = msg + trn(" with {0} inner.", " with {0} inners.", record.getInnersCount(), record.getInnersCount());
-                        } else {
-                            msg = msg + ".";
-                        }
-
-                        TracerUtils.showNotification(msg, "info");
-                        SwingUtilities.invokeLater(new Runnable() {
-                            public void run() {
-                                Main.main.undoRedo.add(new SequenceCommand(tr("Trace object"), commands));
-                            }
-                        });
-
-                        if (m_shift) {
-                            Main.main.getCurrentDataSet().addSelected(record.hasInners()?rel:connectWays.getWay());
-                        } else {
-                            Main.main.getCurrentDataSet().setSelected(record.hasInners()?rel:connectWays.getWay());
-                        }
-                    } else {
-                        System.out.println("Failed");
-                    }
-                }
-            } finally {
-                pm.finishTask();
+                LpisServer server = new LpisServer();
+                m_record = server.getElementBasicData(m_pos, lpisUrl);
+            }
+            catch (Exception e) {
+                m_asyncException = e;
+            }
+            finally {
+                pm.finishTask ();
             }
         }
 
         protected void finish() {
+
+            // Note: finish() is guaranteed to run on EDT, after realRun() suceeded.
+
+            // Async download failed?
+            if (m_asyncException != null) {
+                m_asyncException.printStackTrace();
+                TracerUtils.showNotification(tr("LPIS download failed.") + "\n(" + m_pos.toDisplayString() + ")", "error");
+                return;
+            }
+
+            // No data available?
+            if (m_record.getLpisID() == -1) {
+                TracerUtils.showNotification(tr("Data not available.")+ "\n(" + m_pos.toDisplayString() + ")", "warning");
+                return;
+            }
+
+            try {
+                createTracedPolygon ();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+
+        private void createTracedPolygon() {
+
+            // Note: must be called from finish() only
+
+            System.out.println("  LPIS ID: " + m_record.getLpisID());
+            System.out.println("  LPIS usage: " + m_record.getUsage());
+
+            final List<Bounds> dsBounds = Main.main.getCurrentDataSet().getDataSourceBounds();
+            final Collection<Command> commands = new LinkedList<Command>();
+
+            // Create Outer way
+            Way outer = new Way();
+            Node firstNode = null;
+            // m_record.getCoorCount() - 1 - omit last node
+            for (int i = 0; i < m_record.getOuter().size() - 1; i++) {
+                Node node = new Node(m_record.getOuter().get(i));
+                if (firstNode == null) {
+                    firstNode = node;
+                }
+
+                // Check whether traced node is inside downloaded area
+                int insideCnt = 0;
+                for (Bounds b: dsBounds) {
+                    if (b.contains(node.getCoor())) {
+                        insideCnt++;
+                    }
+                }
+                if (insideCnt == 0) {
+                    ExtendedDialog ed = new ExtendedDialog(
+                        Main.parent, tr("Way is outside downloaded area"),
+                            new String[] {tr("Ok")});
+                    ed.setButtonIcons(new String[] {"ok"});
+                    ed.setIcon(JOptionPane.ERROR_MESSAGE);
+                    ed.setContent(tr("Sorry.\nThe traced way (or part of the way) is outside of the downloaded area.\nPlease download area around the way and try again."));
+                    ed.showDialog();
+                    return;
+                }
+                commands.add(new AddCommand(node));
+                outer.addNode(node);
+            }
+
+            // Insert first node again - close the polygon.
+            outer.addNode(firstNode);
+
+            System.out.println("TracedWay: " + outer.toString());
+            if (!m_record.hasInners()) {
+                tagOuterWay(outer);
+            }
+
+            commands.add(new AddCommand(outer));
+
+            // Inners found - create multipolygon
+            Relation rel = null;
+            if (m_record.hasInners()) {
+                rel = new Relation ();
+                rel.addMember(new RelationMember("outer", outer));
+
+                for (int i = 0; i < m_record.getInnersCount(); i++) {
+                    ArrayList<LatLon> in = m_record.getInner(i);
+                    Way inner = new Way();
+                    firstNode = null;
+                    for (int j = 0; j < in.size() -1 ; j++) {
+                        Node node = new Node(in.get(j));
+                        if (firstNode == null) {
+                            firstNode = node;
+                        }
+                        commands.add(new AddCommand(node));
+                        inner.addNode(node);
+                    }
+
+                    // Insert first node again - close the polygon.
+                    inner.addNode(firstNode);
+
+                    System.out.println("Traced Inner Way: " + inner.toString());
+
+                    commands.add(new AddCommand(inner));
+                    rel.addMember(new RelationMember("inner", inner));
+                }
+
+                // Add relation
+                tagMultipolygon(rel);
+                commands.add(new AddCommand(rel));
+            }
+
+            // connect to other landuse or modify existing landuse
+            ConnectWays connectWays = new ConnectWays();
+            Command connCmd;
+            if (rel != null) {
+                connCmd = connectWays.connect(outer, rel, m_pos, m_ctrl, m_alt, source);
+            } else {
+                connCmd = connectWays.connect(outer, m_pos, m_ctrl, m_alt, source);
+            }
+
+            // nothing changed?
+            String s[] = connCmd.getDescriptionText().split(": ");
+            if (s[1].equals("Nothing")) {
+                TracerUtils.showNotification(tr("Nothing changed."), "info");
+                if (m_shift) {
+                    Main.main.getCurrentDataSet().addSelected(rel != null ? rel : outer);
+                } else {
+                    Main.main.getCurrentDataSet().setSelected(rel != null ? rel : outer);
+                }
+                return;
+            }
+
+            commands.add(connCmd);
+
+            if (!commands.isEmpty()) {
+                String msg = tr("Tracer(LPIS): add an area") + " \"" + m_record.getUsage() + "\"";
+                if (m_record.hasInners()) {
+                    msg = msg + trn(" with {0} inner.", " with {0} inners.", m_record.getInnersCount(), m_record.getInnersCount());
+                } else {
+                    msg = msg + ".";
+                }
+
+                TracerUtils.showNotification(msg, "info");
+
+                Main.main.undoRedo.add(new SequenceCommand(tr("Trace object"), commands));
+
+                if (m_shift) {
+                    Main.main.getCurrentDataSet().addSelected(rel != null ? rel : connectWays.getWay());
+                } else {
+                    Main.main.getCurrentDataSet().setSelected(rel != null ? rel : connectWays.getWay());
+                }
+            } else {
+                System.out.println("Failed");
+            }
         }
 
         protected void cancel() {
             // #### FIXME - resolve cancellation
         }
 
-        private void tagMultipolygon (LpisRecord record, Relation rel) {
-            Map <String, String> map = new HashMap <String, String> (record.getUsageOsm());
+        private void tagMultipolygon (Relation rel) {
+            Map <String, String> map = new HashMap <String, String> (m_record.getUsageOsm());
             map.put("type", "multipolygon");
             map.put("source", source);
-            map.put("ref", Long.toString(record.getLpisID()));
+            map.put("ref", Long.toString(m_record.getLpisID()));
             rel.setKeys(map);
         }
 
-        private void tagOuterWay (LpisRecord record, Way way) {
-            Map <String, String> map = new HashMap <String, String> (record.getUsageOsm());
+        private void tagOuterWay (Way way) {
+            Map <String, String> map = new HashMap <String, String> (m_record.getUsageOsm());
             map.put("source", source);
-            map.put("ref", Long.toString(record.getLpisID()));
+            map.put("ref", Long.toString(m_record.getLpisID()));
             way.setKeys(map);
         }
     }
