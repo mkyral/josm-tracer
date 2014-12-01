mkyral/josm-tracer

commit cf2d6964534bdf1f4db9679dd52738bfecf61d97

Ruian: add support for multipolygon buildings

src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianModule.java
@@ -200,7 +200,7 @@ public class RuianModule implements TracerModule {
                 return;
 
             // No data available?
-            if (m_record.getCoorCount() == 0) {
+            if (m_record.getBuildingID() == -1) {
                 TracerUtils.showNotification(tr("Data not available.")+ "\n(" + m_pos.toDisplayString() + ")", "warning");
                 return;
             }
@@ -419,19 +419,22 @@ public class RuianModule implements TracerModule {
               dAdjY = pref.getRuianAdjustPositionLon();
             }
 
+            IEdNodePredicate reuse_filter = new AreaBoundaryWayNodePredicate(m_reuseExistingBuildingNodeMatch);
+
             // Prepare outer way nodes
-            List<EdNode> outer_nodes = new ArrayList<> ();
             LatLon prev_coor = null;
+            List<LatLon> outer_rls = m_record.getOuter();
+            List<EdNode> outer_nodes = new ArrayList<> (outer_rls.size());
             // m_record.getCoorCount() - 1 - omit last node
-            for (int i = 0; i < m_record.getCoorCount() - 1; i++) {
+            for (int i = 0; i < outer_rls.size() - 1; i++) {
                 EdNode node;
 
                 // Apply corrections to node coordinates
                 if (!pref.isRuianAdjustPositionEnabled()) {
-                  node = editor.newNode(m_record.getCoor(i));
+                  node = editor.newNode(outer_rls.get(i));
                 } else {
-                  node = editor.newNode(new LatLon(LatLon.roundToOsmPrecision(m_record.getCoor(i).lat()+dAdjX),
-                                                   LatLon.roundToOsmPrecision(m_record.getCoor(i).lon()+dAdjY)));
+                  node = editor.newNode(new LatLon(LatLon.roundToOsmPrecision(outer_rls.get(i).lat()+dAdjX),
+                                                   LatLon.roundToOsmPrecision(outer_rls.get(i).lon()+dAdjY)));
                 }
 
                 if (!editor.insideDataSourceBounds(node)) {
@@ -450,33 +453,46 @@ public class RuianModule implements TracerModule {
             // Close & create outer way
             outer_nodes.add(outer_nodes.get(0));
             EdWay outer_way = editor.newWay(outer_nodes);
+            outer_way.reuseExistingNodes(gconn, reuse_filter);
 
-// #### Multipolygons are not supported yet.
             // Simple way?
-//             if (!m_record.hasInners())
+            if (!m_record.hasInners())
                 return new Pair<>(outer_way, null);
 
-//             // Create multipolygon
-//             EdMultipolygon multipolygon = editor.newMultipolygon();
-//             multipolygon.addOuterWay(outer_way);
-//
-//             for (List<LatLon> inner_lls: m_record.getInners()) {
-//                 List<EdNode> inner_nodes = new ArrayList<>(inner_lls.size());
-//                 for (int i = 0; i < inner_lls.size() - 1; i++) {
-//                     inner_nodes.add(editor.newNode(inner_lls.get(i)));
-//                 }
-//
-//                 // Close & create inner way
-//                 if (inner_nodes.size() < 3)
-//                     throw new AssertionError(tr("Inner way consists of less than 3 nodes"));
-//                 inner_nodes.add(inner_nodes.get(0));
-//                 EdWay way = editor.newWay(inner_nodes);
-//                 way.reuseNearNodes(gconn, reuse_filter, true);
-//
-//                 multipolygon.addInnerWay(way);
-//             }
-//
-//             return new Pair<>(outer_way, multipolygon);
+            // Create multipolygon
+            prev_coor = null;
+            EdMultipolygon multipolygon = editor.newMultipolygon();
+            multipolygon.addOuterWay(outer_way);
+
+            for (List<LatLon> inner_rls: m_record.getInners()) {
+                List<EdNode> inner_nodes = new ArrayList<>(inner_rls.size());
+                for (int i = 0; i < inner_rls.size() - 1; i++) {
+                    EdNode node;
+                    // Apply corrections to node coordinates
+                    if (!pref.isRuianAdjustPositionEnabled()) {
+                      node = editor.newNode(inner_rls.get(i));
+                    } else {
+                      node = editor.newNode(new LatLon(LatLon.roundToOsmPrecision(inner_rls.get(i).lat()+dAdjX),
+                                                      LatLon.roundToOsmPrecision(inner_rls.get(i).lon()+dAdjY)));
+                    }
+
+                    if (!gconn.duplicateNodes(node.getCoor(), prev_coor)) {
+                        inner_nodes.add(node);
+                        prev_coor = node.getCoor();
+                    }
+                }
+
+                // Close & create inner way
+                if (inner_nodes.size() < 3)
+                    throw new AssertionError(tr("Inner way consists of less than 3 nodes"));
+                inner_nodes.add(inner_nodes.get(0));
+                EdWay way = editor.newWay(inner_nodes);
+                way.reuseExistingNodes(gconn, reuse_filter);
+
+                multipolygon.addInnerWay(way);
+            }
+
+            return new Pair<>(outer_way, multipolygon);
         }
 
         private Pair<EdObject, Boolean> getObjectToRetrace(WayEditor editor, LatLon pos, Match retraceAreaMatch) {

servers/ruianBuildings/index.php
@@ -2,6 +2,8 @@
 require("config.php");
 $lat=$_REQUEST['lat'];
 $lon=$_REQUEST['lon'];
+$req=$_REQUEST['req'];
+
 if ( !is_numeric($lat) or !is_numeric($lon) ) die;
 header('Content-Type: application/json');
 
@@ -9,6 +11,7 @@ $data = array();
 
 $data["coordinates"] = array( "lat" => "$lat", "lon" => "$lon");
 $data["source"] = "cuzk:ruian";
+
 
 // building
 $query="
@@ -71,29 +74,65 @@ if (pg_num_rows($result) > 0)
 
   // -----------------
   // Building geometry
-  $query="
-    select st_asgeojson(st_transform(local_simplify_polygon(xhranice),4326)) as geom
-    from
-      (select kod,(st_dump(hranice)).geom as xhranice
-      from rn_stavebni_objekt
-      where st_contains(hranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
-        and not deleted) as foo
-    where st_contains(xhranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
-    limit 1
-  ";
+  if ( "$req" == "full" )
+  { // export complete building including inners
+    $query="
+      select st_asgeojson(st_transform(local_less_simplify_polygon((st_dumprings(xhranice)).geom),4326)) as geom
+      from
+        (select kod,(st_dump(hranice)).geom as xhranice
+        from rn_stavebni_objekt
+        where st_contains(hranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+          and not deleted) as foo
+      where st_contains(xhranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+    ";
+  } else
+  { // Export only outer way
+    $query="
+      select st_asgeojson(st_transform(local_simplify_polygon(xhranice),4326)) as geom
+      from
+        (select kod,(st_dump(hranice)).geom as xhranice
+        from rn_stavebni_objekt
+        where st_contains(hranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+          and not deleted) as foo
+      where st_contains(xhranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+      limit 1
+    ";
+  }
   $result = pg_query($CONNECT,$query);
 
-  if (pg_num_rows($result) > 0)
+  if (pg_num_rows($result) == 1 && !"$req" == "full")
   {
+
     $geom = pg_result($result,0,"geom");
     $geometry=json_decode($geom,true);
     $data["geometry"] = $geometry['coordinates'][0];
   }
-  else
+  else if (pg_num_rows($result) > 1 || "$req" == "full")
+  {
+    $geom = array();
+    $inners = array();
+    for ($i = 0; $i < pg_num_rows($result); $i++)
+    {
+      $row = pg_result($result,$i,"geom");
+      $geometry=json_decode($row,true);
+      if ($i == 0)
+      {
+        $geom['outer'] =$geometry['coordinates'][0];
+      } else if ($geometry['coordinates'][0][0] == $geom['outer'][0])
+      {
+        // Ignore dulicated outer ways on the same position
+        break;
+      } else
+      {
+        array_push($inners, $geometry['coordinates'][0]);
+      }
+    }
+      $geom['inners'] = $inners;
+      $data["geometry"] = $geom;
+  } else
   {
     $data["geometry"] = array();
   }
-
   // -----------------
   // Addresses
   $query="

src/org/openstreetmap/josm/plugins/tracer/modules/lpis/LpisModule.java
@@ -464,7 +464,7 @@ public class LpisModule implements TracerModule  {
             IEdNodePredicate landuse_filter = new AreaBoundaryWayNodePredicate(m_reuseExistingLanduseNodeMatch);
             IEdNodePredicate exclude_my_nodes = new ExcludeEdNodesPredicate(obj);
             IEdNodePredicate filter = new EdNodeLogicalAndPredicate (exclude_my_nodes, landuse_filter);
-            
+
             obj.connectExistingTouchingNodes(gconn, filter);
         }
     }

src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianRecord.java
@@ -57,7 +57,8 @@ public class RuianRecord {
     private String   m_finished;
     private String   m_valid_from;
 
-    private ArrayList <LatLon> m_geometry;
+    private List<LatLon> m_outer;
+    private List<List<LatLon>> m_inners;
     private ArrayList <Address> m_address_places;
 
     /**
@@ -77,7 +78,7 @@ public class RuianRecord {
       m_coor_lat = 0;
       m_coor_lon = 0;
       m_source = "";
-      m_ruian_id = 0;
+      m_ruian_id = -1;
       m_levels = 0;
       m_flats = 0;
       m_usage_code = "";
@@ -85,7 +86,8 @@ public class RuianRecord {
       m_usage_val = "";
       m_finished = "";
       m_valid_from = "";
-      m_geometry = new ArrayList<LatLon> ();
+      m_outer = new ArrayList<>();
+      m_inners = new ArrayList<>();
       m_address_places = new ArrayList<Address> ();
 
     }
@@ -265,28 +267,56 @@ public class RuianRecord {
 
 // =========================================================================
       try {
-        JsonArray arr = obj.getJsonArray("geometry");
+        JsonObject geomObj = obj.getJsonObject("geometry");
 
-        for(int i = 0; i < arr.size(); i++)
+        // Outer
+        JsonArray outerArr = geomObj.getJsonArray("outer");
+        for(int i = 0; i < outerArr.size(); i++)
         {
-          System.out.println("i="+i);
-          JsonArray node = arr.getJsonArray(i);
+          JsonArray node = outerArr.getJsonArray(i);
 
           try {
             LatLon coor = new LatLon(
               LatLon.roundToOsmPrecision(node.getJsonNumber(1).doubleValue()),
               LatLon.roundToOsmPrecision(node.getJsonNumber(0).doubleValue())
             );
-            System.out.println("coor: " + coor.toString());
-            m_geometry.add(coor);
+            System.out.println("outer["+i+"]:coor: " + coor.toString());
+            m_outer.add(coor);
           } catch (Exception e) {
           }
+        }
 
+        try {
+          // Inners
+          JsonArray innersArr = geomObj.getJsonArray("inners");
+          for(int i = 0; i < innersArr.size(); i++)
+          {
+            JsonArray innerArr = innersArr.getJsonArray(i);
+            ArrayList<LatLon> inner = new ArrayList<>();
+
+            System.out.println("");
+            for(int j = 0; j < innerArr.size(); j++)
+            {
+              JsonArray node = innerArr.getJsonArray(j);
+
+              try {
+                LatLon coor = new LatLon(
+                  LatLon.roundToOsmPrecision(node.getJsonNumber(1).doubleValue()),
+                  LatLon.roundToOsmPrecision(node.getJsonNumber(0).doubleValue())
+                );
+                System.out.println("inner["+i+"]["+j+"]:coor: " + coor.toString());
+                inner.add(coor);
+              } catch (Exception e) {
+              }
+            }
+            m_inners.add(inner);
+          }
+        } catch (Exception e) {
         }
       } catch (Exception e) {
       }
 
-// =========================================================================
+      // =========================================================================
       try {
         JsonArray arr = obj.getJsonArray("adresni_mista");
 
@@ -366,35 +396,42 @@ public class RuianRecord {
       }
     }
 
-  /**
-   *  Return number of nodes in the building
-   *  @return Count of nodes in building
-   */
-  public int getCoorCount() {
-    if (m_geometry == null)
-      return 0;
-    else
-      return m_geometry.size();
-  }
+    /**
+     *  Return outer polygon
+     *  @return Outer polygon nodes
+     */
+    public List <LatLon> getOuter() {
+        if (m_outer == null)
+            throw new IllegalStateException("No outer geometry available");
+        return Collections.unmodifiableList(m_outer);
+    }
 
-  /**
-   *  Return coordinates of node
-   *  @return geometry node coordinates
-   */
-  public LatLon getCoor(int i) {
-    return m_geometry.get(i);
-  }
+    /**
+     *  Return whether there are inners
+     *  @return True/False
+     */
+    public boolean hasInners() {
+        return m_inners.size() > 0;
+    }
+
+    /**
+     *  Return number of inners
+     *  @return List of inners
+     */
+    public List<List<LatLon>> getInners() {
+        return Collections.unmodifiableList(m_inners);
+    }
 
   /**
    * Returns BBox of RUIAN (multi)polygon
    * @return BBox of RUIAN (multi)polygon
    */
   public BBox getBBox() {
-      LatLon p0 = m_geometry.get(0);
+      LatLon p0 = m_outer.get(0);
 
       BBox bbox = new BBox(p0.lon(), p0.lat());
-      for (int i = 1; i < m_geometry.size(); i++) {
-          LatLon p = m_geometry.get(i);
+      for (int i = 1; i < m_outer.size(); i++) {
+          LatLon p = m_outer.get(i);
           bbox.add(p.lon(), p.lat());
       }
 

src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianServer.java
@@ -65,8 +65,9 @@ public class RuianServer {
      */
     public RuianRecord trace(LatLon pos, String url) {
         try {
-            System.out.println("Request: "+ url + "/ruian-buildings/?lat=" + pos.lat() + "&lon=" + pos.lon());
-            String content = callServer(url + "/ruian-buildings/?lat=" + pos.lat() + "&lon=" + pos.lon());
+            String call_url = url + "/ruian-buildings/?req=full&lat=" + pos.lat() + "&lon=" + pos.lon();
+            System.out.println("Request: " + call_url);
+            String content = callServer(call_url);
             System.out.println("Reply: " + content);
             RuianRecord ruian = new RuianRecord();
             ruian.parseJSON(content);

commit ff1ebf48de6065913dfeed827522094caf1d1f99

RUIAN: fix double reuse of existing nodes
Existing (near) nodes must be reused only once. Second call
leads to duplicate nodes if there's another near node available.
This can easily happen around a junction point of two or more
inaccurate buildings, e.g. series of garages.

This bug was introduced in cf2d6964534bdf1f4db9679dd52738bfecf61d97.

src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianModule.java
@@ -518,7 +518,6 @@ public class RuianModule implements TracerModule {
             // Close & create outer way
             outer_nodes.add(outer_nodes.get(0));
             EdWay outer_way = editor.newWay(outer_nodes);
-            outer_way.reuseExistingNodes(reuse_filter);
 
             // Simple way?
             if (!m_record.hasInners())
@@ -552,7 +551,6 @@ public class RuianModule implements TracerModule {
                     throw new AssertionError(tr("Inner way consists of less than 3 nodes"));
                 inner_nodes.add(inner_nodes.get(0));
                 EdWay way = editor.newWay(inner_nodes);
-                way.reuseExistingNodes(reuse_filter);
 
                 multipolygon.addInnerWay(way);
             }