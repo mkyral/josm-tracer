3ebec9cbf212aa917577f2bd21c5e6ae80e46773
Martin Svec <osm@maatts.cz>
2014-12-23 18:45:10 +0100

    RUIAN: Don't ignore RuianServer exceptions, free resources

--- src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianServer.java
+++ src/org/openstreetmap/josm/plugins/tracer/modules/ruian/RuianServer.java
@@ -20,60 +20,55 @@
 package org.openstreetmap.josm.plugins.tracer.modules.ruian;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import org.openstreetmap.josm.data.coor.LatLon;
 
-// import org.openstreetmap.josm.plugins.tracer.RuianRecord;
-
-public class RuianServer {
-
+public final class RuianServer {
 
     public RuianServer() {
-
     }
 
     /**
      * Call Trace server.
+     *
      * @param urlString Input parameters.
      * @return Result text.
      */
-    private String callServer(String urlString) {
-        try {
-            URL url = new URL(urlString);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-
+    private String callServer(String urlString) throws MalformedURLException, IOException {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new URL(urlString).openStream()))) {
             StringBuilder sb = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                if (sb.length() == 0)
-                  sb.append(line);
-                else
-                  sb.append(" "+line);
+                if (sb.length() > 0) {
+                    sb.append(" ");
+                }
+                sb.append(line);
             }
             return sb.toString();
-        } catch (Exception e) {
-            return "";
         }
     }
 
     /**
-     * Trace building on position.
+     * Trace building on given position.
+     *
      * @param pos Position of building.
-     * @return Building border.
+     * @param url Url of RUIAN server
+     * @param adjlat Latitude correction to be applied to building geometry.
+     * @param adjlon Longitude correction to be applied to building geometry.
+     * @return Building data.
+     * @throws java.io.IOException
      */
-    public RuianRecord trace(LatLon pos, String url, double adjlat, double adjlon) {
-        try {
-            String call_url = url + "/ruian-buildings/?req=full&lat=" + pos.lat() + "&lon=" + pos.lon();
-            System.out.println("Request: " + call_url);
-            String content = callServer(call_url);
-            System.out.println("Reply: " + content);
-            RuianRecord ruian = new RuianRecord(adjlat, adjlon);
-            ruian.parseJSON(content);
-            return ruian;
-        } catch (Exception e) {
-            return new RuianRecord(adjlat, adjlon);
-        }
+    public RuianRecord trace(LatLon pos, String url, double adjlat, double adjlon) throws IOException {
+        String call_url = url + "/ruian-buildings/?req=full&lat=" + pos.lat() + "&lon=" + pos.lon();
+        System.out.println("Request: " + call_url);
+        String content = callServer(call_url);
+        System.out.println("Reply: " + content);
+        RuianRecord ruian = new RuianRecord(adjlat, adjlon);
+        ruian.parseJSON(content);
+        return ruian;
     }
 }
