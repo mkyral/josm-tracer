16056f1f94aee338a1d95828f4759147e1d42ed5
Martin Svec <osm@maatts.cz>
2014-12-24 12:26:10 +0100

    Cleanup TracerAction class

--- src/org/openstreetmap/josm/plugins/tracer/TracerAction.java
+++ src/org/openstreetmap/josm/plugins/tracer/TracerAction.java
@@ -19,50 +19,28 @@
 package org.openstreetmap.josm.plugins.tracer;
 
 import static org.openstreetmap.josm.tools.I18n.*;
-import java.awt.Cursor;
 import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.mapmode.MapMode;
-import org.openstreetmap.josm.command.AddCommand;
-import org.openstreetmap.josm.command.Command;
-import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.coor.LatLon;
-import org.openstreetmap.josm.data.osm.Node;
-import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.PleaseWaitRunnable;
-import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.gui.MapFrame;
-import org.openstreetmap.josm.plugins.tracer.TracerPreferences;
-import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Shortcut;
-import org.xml.sax.SAXException;
 
-class TracerAction extends MapMode implements MouseListener, KeyListener{
+class TracerAction extends MapMode implements MouseListener, KeyListener {
 
     private static final long serialVersionUID = 1L;
-
-    protected boolean cancel;
-    private boolean ctrl;
-    private boolean alt;
-    private boolean shift;
-
-    private Modules m_modules = new Modules();
-//     private TracerUtils tu = new TracerUtils();
+    private final Modules m_modules = new Modules();
 
     public TracerAction(MapFrame mapFrame) {
-        super(tr("Tracer"), "tracer-sml", tr("Tracer."), Shortcut.registerShortcut("tools:tracer", tr("Tool: {0}", tr("Tracer")), KeyEvent.VK_T, Shortcut.DIRECT), mapFrame, null);
+        super(tr("Tracer"), "tracer-sml", tr("Tracer."),
+                Shortcut.registerShortcut("tools:tracer", tr("Tool: {0}", tr("Tracer")), KeyEvent.VK_T, Shortcut.DIRECT), mapFrame, null);
     }
-
 
     @Override
     public void enterMode() {
@@ -72,8 +50,8 @@ class TracerAction extends MapMode implements MouseListener, KeyListener{
 
         m_modules.refreshModulesStatus();
         if (m_modules.getActiveModulesCount() == 0) {
-          TracerUtils.showNotification(tr("Tracer: No active module found!\nPlease enable some in configuration."), "error");
-          return;
+            TracerUtils.showNotification(tr("Tracer: No active module found!\nPlease enable some in configuration."), "error");
+            return;
         }
 
         super.enterMode();
@@ -92,69 +70,37 @@ class TracerAction extends MapMode implements MouseListener, KeyListener{
     }
 
     @Override
-    public void keyReleased ( KeyEvent e ) {
+    public void keyReleased(KeyEvent e) {
     }
 
     @Override
-    public void keyTyped ( KeyEvent e ) {
-      checkKey(e);
+    public void keyTyped(KeyEvent e) {
+        checkKey(e);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-      checkKey(e);
+        checkKey(e);
     }
 
     private void checkKey(KeyEvent e) {
-      String previousModule = m_modules.getActiveModuleName();
+        String previousModule = m_modules.getActiveModuleName();
 
-      switch (e.getKeyCode()) {
-      case KeyEvent.VK_T : // T
-        m_modules.nextModule();
-        if (! previousModule.equals(m_modules.getActiveModuleName())) {
-          TracerUtils.showNotification(tr("Tracer: Switched to {0} module.", m_modules.getActiveModuleName()), "info", 700);
-          Main.map.mapView.setCursor(m_modules.getActiveModule().getCursor());
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_T: // T
+                m_modules.nextModule();
+                if (!previousModule.equals(m_modules.getActiveModuleName())) {
+                    TracerUtils.showNotification(tr("Tracer: Switched to {0} module.", m_modules.getActiveModuleName()), "info", 700);
+                    Main.map.mapView.setCursor(m_modules.getActiveModule().getCursor());
+                }
+                break;
         }
-        break;
-      default:
-        return;
-      }
     }
 
     protected void traceAsync(Point clickPoint) {
-        cancel = false;
-        /**
-         * Positional data
-         */
         final LatLon pos = Main.map.mapView.getLatLon(clickPoint.x, clickPoint.y);
-
-        try {
-/*            PleaseWaitRunnable tracerTask = new PleaseWaitRunnable(tr("Tracing")) {
-
-                @Override
-                protected void realRun() throws SAXException {
-                    m_modules.getActiveModule().trace(pos, ctrl, alt, shift, progressMonitor.createSubTaskMonitor(ProgressMonitor.ALL_TICKS, false));
-                }
-
-                @Override
-                protected void finish() {
-                }
-
-                @Override
-                protected void cancel() {
-                    TracerAction.this.cancel();
-                }
-            };
-            Main.worker.submit (tracerTask);*/
-            PleaseWaitRunnable tracerTask = m_modules.getActiveModule().trace(pos, ctrl, alt, shift);
-            Main.worker.submit (tracerTask);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void cancel() {
-        cancel = true;
+        PleaseWaitRunnable tracerTask = m_modules.getActiveModule().trace(pos, ctrl, alt, shift);
+        Main.worker.submit(tracerTask);
     }
 
     @Override
@@ -183,15 +129,7 @@ class TracerAction extends MapMode implements MouseListener, KeyListener{
     }
 
     @Override
-    protected void updateKeyModifiers(MouseEvent e) {
-        ctrl = (e.getModifiers() & ActionEvent.CTRL_MASK) != 0;
-        alt = (e.getModifiers() & (ActionEvent.ALT_MASK | InputEvent.ALT_GRAPH_MASK)) != 0;
-        shift = (e.getModifiers() & ActionEvent.SHIFT_MASK) != 0;
-    }
-
-    @Override
     public void mouseReleased(MouseEvent e) {
         Main.map.mapView.setCursor(m_modules.getActiveModule().getCursor());
     }
 }
-
