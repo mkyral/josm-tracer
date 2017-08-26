3f76932853780bfb45dcc09494b75245b26b08b0
Marián Kyral <mkyral@email.cz>
2017-08-26 18:29:43 +0200

    Replace depricated Main.main.menu by MainApplication.getMenu()

--- src/org/openstreetmap/josm/plugins/tracer/TracerPlugin.java
+++ src/org/openstreetmap/josm/plugins/tracer/TracerPlugin.java
@@ -19,7 +19,7 @@
 
 package org.openstreetmap.josm.plugins.tracer;
 
-import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.plugins.Plugin;
@@ -29,7 +29,7 @@ public class TracerPlugin extends Plugin {
 
     public TracerPlugin(PluginInformation info) {
         super(info);
-        MainMenu.add(Main.main.menu.moreToolsMenu, new TracerAction());
+        MainMenu.add(MainApplication.getMenu().moreToolsMenu, new TracerAction());
     }
 
     @Override
