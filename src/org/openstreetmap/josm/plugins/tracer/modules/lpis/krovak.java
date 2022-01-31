22235cec405938bd82bace81d0c9be6059dbb1f4
Marián Kyral <mkyral@email.cz>
2022-01-31 17:44:38 +0100

    Fix Krovak - WGS84 coordinates transformation issue with latest geotools

--- src/org/openstreetmap/josm/plugins/tracer/modules/lpis/krovak.java
+++ src/org/openstreetmap/josm/plugins/tracer/modules/lpis/krovak.java
@@ -45,28 +45,6 @@ public class krovak {
   private CoordinateReferenceSystem krovakCRS;
 
 
-  /* Define Krovak projection */
-  private String wkt = "PROJCS[\"S-JTSK / Krovak East North\","+
-      "GEOGCS[\"S-JTSK\","+
-      "  DATUM[\"System_Jednotne_Trigonometricke_Site_Katastralni\","+
-      "    SPHEROID[\"Bessel 1841\",6377397.155,299.1528128,AUTHORITY[\"EPSG\",\"7004\"]],"+
-      "    TOWGS84[570.8,85.7,462.8,4.998,1.587,5.261,3.56],AUTHORITY[\"EPSG\",\"6156\"]],"+
-      "  PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],"+
-      "  UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],"+
-      "  AUTHORITY[\"EPSG\",\"4156\"]],"+
-      "  PROJECTION[\"Krovak\"],"+
-      "PARAMETER[\"latitude_of_center\",49.5],"+
-      "PARAMETER[\"longitude_of_center\",24.83333333333333],"+
-      "PARAMETER[\"azimuth\",30.28813972222222],"+
-      "PARAMETER[\"pseudo_standard_parallel_1\",78.5],"+
-      "PARAMETER[\"scale_factor\",0.9999],"+
-      "PARAMETER[\"false_easting\",0],"+
-      "PARAMETER[\"false_northing\",0],"+
-      "UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],"+
-      "AXIS[\"X\",EAST],"+
-      "AXIS[\"Y\",NORTH],"+
-      "AUTHORITY[\"EPSG\",\"5514\"]]";
-
   public krovak () {
     init();
   }
@@ -77,7 +55,7 @@ public class krovak {
 
     try {
       LatLonCRS = DefaultGeographicCRS.WGS84;
-      krovakCRS = CRS.parseWKT(wkt);
+      krovakCRS = CRS.decode("EPSG:5514", false);
 
     } catch (Exception e) {
       System.out.println("krovak init() exception: " + e.getMessage());
@@ -102,7 +80,7 @@ public class krovak {
 
       org.locationtech.jts.geom.Point p = gf.createPoint(c);
 
-      MathTransform mathTransform = CRS.findMathTransform(krovakCRS, LatLonCRS, false);
+      MathTransform mathTransform = CRS.findMathTransform(krovakCRS, LatLonCRS);
       org.locationtech.jts.geom.Point p1 = (org.locationtech.jts.geom.Point) JTS.transform(p, mathTransform);
 
 //       System.out.println(p1.getCoordinate());
@@ -125,7 +103,7 @@ public class krovak {
 
       org.locationtech.jts.geom.Point p = gf.createPoint(c);
 
-      MathTransform mathTransform = CRS.findMathTransform(LatLonCRS, krovakCRS, false);
+      MathTransform mathTransform = CRS.findMathTransform(LatLonCRS, krovakCRS);
       org.locationtech.jts.geom.Point p1 = (org.locationtech.jts.geom.Point) JTS.transform(p, mathTransform);
 
 //       System.out.println(p1.getCoordinate());
